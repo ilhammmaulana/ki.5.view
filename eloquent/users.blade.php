a80c6dea310fbfe613be9207adc736fc88cc24f3
ilhammmaulana <ilhammmaulana688@gmail.com>
2023-12-16 10:14:07 +0700

    fix: name elequent to eloquent

--- eloquent/users.blade.php
+++ eloquent/users.blade.php
@@ -1,17 +1,22 @@
 <!doctype html>
 <html lang="en">
+
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Users</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>Users - Eloquent</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+      integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   </head>
+
   <body>
-    <h1>Users table</h1>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <h1>Users table - Eloquent</h1>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+    </script>
     <div class="container">
-<a href="{{ route('elequent.create') }}" class="btn btn-primary">Create</a>
-    <table class="table">
+      <a href="{{ route('eloquent.create') }}" class="btn btn-primary">Create</a>
+      <table class="table">
         <thead>
           <tr>
             <th scope="col">id</th>
@@ -19,28 +24,29 @@
             <th scope="col">Email</th>
             <th scope="col">Created at</th>
             <th scope="col">Action</th>
-            
+
           </tr>
         </thead>
         <tbody>
           @foreach ($users as $user)
-                <tr>
-                    <th scope="row">{{ $user->id }}</th>
-                    <td>{{ $user->name }}</td>
-                    <td>{{ $user->email }}</td>
-                    <td>{{ $user->created_at }}</td>
-                    <td>
-                        <a href="{{ route('elequent.edit', $user->id) }}" class="btn btn-warning">Edit</a>
-                        <form action="{{ route('elequent.destroy', $user->id) }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-danger">Delete</button>
-                        </form>
-                    </td>
-                </tr>
+          <tr>
+            <th scope="row">{{ $user->id }}</th>
+            <td>{{ $user->name }}</td>
+            <td>{{ $user->email }}</td>
+            <td>{{ $user->created_at }}</td>
+            <td>
+              <a href="{{ route('eloquent.edit', $user->id) }}" class="btn btn-warning">Edit</a>
+              <form action="{{ route('eloquent.destroy', $user->id) }}" method="POST">
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="btn btn-danger">Delete</button>
+              </form>
+            </td>
+          </tr>
           @endforeach
         </tbody>
       </table>
-    </div>    
+    </div>
   </body>
+
 </html>
